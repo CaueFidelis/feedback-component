8743d0908e4787e254e94b3777792799a27a9022
Caue <cauefidelis@live.com>
2022-09-11 21:25:06 -0300

    Removing not used imports

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,8 +2,6 @@ import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-ada
 import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
 import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 import express from 'express';
-import { prisma } from './prisma';
-import nodemailer from 'nodemailer';
 
 export const routes = express.Router();
 
